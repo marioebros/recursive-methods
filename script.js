14f3bc96604373576ea75b033700533131753f4d
Mario M. Elizalde <dollarize.mme@gmail.com>
2022-11-04 14:30:41 -0700

    Created example skeleton to begin recur

---
+++ script.js
@@ -0,0 +1,20 @@
+function printChildren(t) {
+  // ???
+}
+
+const tree = {
+  name: "Johnny",
+  children: [
+    {
+      name: "Jimmy",
+      children: [],
+    },
+    {
+      name: "Zoe",
+      children: [
+        { name: "Kyle", children: [] },
+        { name: "Sophia", children: [] },
+      ],
+    },
+  ],
+};
